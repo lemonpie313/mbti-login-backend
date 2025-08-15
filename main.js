94592d49fec81a6cc7948e6192f9fe031ac60044
SuhyeonJin <lofi0613@gmail.com>
2025-08-15 15:58:47 +0900

    init

---
+++ main.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const USERNAME = process.env.LOGIN_ID;
+const PASSWORD = process.env.LOGIN_PW;
+const SECRET_KEY = process.env.JWT_SECRET || "temp_secret";
+
+app.use(bodyParser.json());
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === USERNAME && password === PASSWORD) {
+    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
+    return res.json({ token });
+  }
+
+  res.status(401).json({ message: "Invalid credentials" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running`);
+});
